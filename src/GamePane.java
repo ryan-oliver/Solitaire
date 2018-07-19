34677db9e421d79862d64f5ce57c2bbdf93a3eff
Ryan <ryoliver7@gmail.com>
2018-07-19 11:46:59 -0400

    .

--- src/GamePane.java
+++ src/GamePane.java
@@ -12,6 +12,8 @@ public class GamePane extends Pane {
     static Pane gameBoard;
     private static boolean gameStarted = false; // Boolean to know if game has started
     static int num = 1; // Used to make the piles for the game board. Set pile number when created
+    static ArrayList<Integer> randCard;
+    static ArrayList<Integer> randTab;
 
     GamePane() {
     }
@@ -32,24 +34,24 @@ public class GamePane extends Pane {
     /** Add piles to pane **/
     static void getPiles() {
         Pile.piles = new ArrayList<>();
-        for(int i = 0, a = 1, n = 220; i < 4; i++, num++, a += 13) { // a = ace card num
-            Pile.piles.add(new FoundationPile(n += 130, 25, num));
+        for(int i = 0, a = 1, n = -115; i < 4; i++, num++, a += 13) { // a = ace card num
+            Pile.piles.add(new FoundationPile(1050, n += 175, num));
             Pile.piles.get(i).setIsFoundationPile(true);
             gameBoard.getChildren().add(Pile.piles.get(i).getRectangle());
-            Card card = new Card(String.valueOf(0), i + 1);
+            Card card = new Card(String.valueOf(0), i + 1); // Invisible cards that allow ace to transfer to foundations
             card.setFill(Color.TRANSPARENT);
             Pile.makeDraggable(card);
             Pile.piles.get(i).addCard(card);
         }
         num = 5;
-        for (int t = 4, n = 32; t < 11; t++, num++) { // loops set x location before adding to gameBoard. n = x location
-            Pile.piles.add(new TableauPile(n += 130, 210, num)); // creates and adds TableauPile object to Tableau.tableauPiles array
+        for (int t = 4, n = 0; t < 11; t++, num++) { // loops set x location before adding to gameBoard. n = x location
+            Pile.piles.add(new TableauPile(n += 130, 80, num)); // creates and adds TableauPile object to Tableau.tableauPiles array
             gameBoard.getChildren().add(Pile.piles.get(t).getRectangle()); // add pile to the gameBoard
         }
 
         // second row
-        for(int t = 11, n = 97; t <= 16; t++, num++) {
-            Pile.piles.add(new TableauPile(n += 130, 500, num));
+        for(int t = 11, n = 67; t <= 16; t++, num++) {
+            Pile.piles.add(new TableauPile(n += 130, 440, num));
             gameBoard.getChildren().add(Pile.piles.get(t).getRectangle());
         }
     }
@@ -63,7 +65,7 @@ public class GamePane extends Pane {
         Deck.getCards();
 
         // Make array list for random numbers
-        ArrayList<Integer> randTab = new ArrayList<>();
+        randTab = new ArrayList<>();
         for(int i = 4; i <= 16; i++)
             randTab.add(i);
         Collections.shuffle(randTab); // Shuffle the deck
@@ -81,7 +83,7 @@ public class GamePane extends Pane {
             }
             kings = true;
         }
-        ArrayList<Integer> randCard = new ArrayList<>();
+        randCard = new ArrayList<>();
         for(int i = 0; i < 51; i++)
             randCard.add(i);
         Collections.shuffle(randCard);
@@ -111,17 +113,53 @@ public class GamePane extends Pane {
         getButtons();
 
         // Deal new deck
-        Deck.getCards();
         deal();
+    }
+
+    static void restartGame() {
+        gameBoard.getChildren().clear();
+        // Clear tableau, foundations, and deck arrays
+        Pile.piles.clear();
+
+        num = 1;
+
+        getPiles();
+        getButtons();
+
+        // Deal cards kings first
+        int cardCount = 0;
+        int tableauRandomPileNum = 0;
+        boolean kings = false;
+        while(!kings) {
+            for (int k = 12; k <= 51; k += 13) {
+                int tableauPileNumForKing = randTab.get(tableauRandomPileNum++);
+                Pile.piles.get(tableauPileNumForKing).dealCards(Deck.masterDeck.get(k));
+                gameBoard.getChildren().add(Deck.masterDeck.get(k).getCardImage()); // Add card image to gameBoard
+                Deck.masterDeck.get(k).setInTableau(true);
+            }
+            kings = true;
+        }
+        for(int t = 4; t < 17; t++) {
+            while (Pile.piles.get(t).getPileSize() < 4) {
+                if (randCard.get(cardCount) != 12 && randCard.get(cardCount) != 25 && randCard.get(cardCount) != 38 && randCard.get(cardCount) != 51) {
+                    Pile.piles.get(t).dealCards(Deck.masterDeck.get(randCard.get(cardCount)));
+                    gameBoard.getChildren().add(Deck.masterDeck.get(randCard.get(cardCount)).getCardImage()); // Add card image to gameBoard
+                    Deck.masterDeck.get(cardCount).setInTableau(true);
+                }
+                cardCount++;
+            }
+            Pile.piles.get(t).getTopCard().setIsTopCard(true);
+        }
     }
 
     /** Add buttons **/
     static void getButtons() {
         // Start button
         Button start = new Button("Start");
-        start.setLayoutX(50);
+        start.setLayoutX(15);
         start.setLayoutY(30);
-        start.setStyle("-fx-border-color: #006400; -fx-border-width: 4px;");
+        start.setStyle("-fx-border-color: #006400; -fx-border-width: 4px; -fx-font: 12 arial;");
+        start.setMinWidth(85);
         gameBoard.getChildren().add(start);
         start.setOnAction((e) -> {
             if (!gameStarted) { // Prevents start button use after game start
@@ -131,9 +169,10 @@ public class GamePane extends Pane {
 
         // Undo button
         Button undo = new Button("Undo/Redo");
-        undo.setLayoutX(50); // X and Y locations of button
+        undo.setLayoutX(15); // X and Y locations of button
         undo.setLayoutY(70);
-        undo.setStyle("-fx-border-color: #006400; -fx-border-width: 4px;");
+        undo.setStyle("-fx-border-color: #006400; -fx-border-width: 4px; -fx-font: 12 arial;");
+        undo.setMinWidth(85);
         gameBoard.getChildren().add(undo);
         undo.setOnAction(e -> {
             if (gameStarted) {
@@ -145,13 +184,27 @@ public class GamePane extends Pane {
 
         // New Game button
         Button newGame = new Button("New Game");
-        newGame.setLayoutX(50);
+        newGame.setLayoutX(15);
         newGame.setLayoutY(110);
-        newGame.setStyle("-fx-border-color: #006400; -fx-border-width: 4px;");
+        newGame.setStyle("-fx-border-color: #006400; -fx-border-width: 4px; -fx-font: 12 arial;");
+        newGame.setMinWidth(85);
         gameBoard.getChildren().add(newGame);
         newGame.setOnAction((e) -> {
             if (gameStarted) { // prevents restart button use if game has not started
                 GamePane.newGame();
+            }
+        });
+
+        // Restart Game button
+        Button restartGame = new Button( "Restart");
+        restartGame.setLayoutX(15);
+        restartGame.setLayoutY(150);
+        restartGame.setStyle("-fx-border-color: #006400; -fx-border-width: 4px; -fx-font: 12 arial;");
+        restartGame.setMinWidth(85);
+        gameBoard.getChildren().add(restartGame);
+        restartGame.setOnAction((e) -> {
+            if (gameStarted) { // prevents restart button use if game has not started
+                GamePane.restartGame();
             }
         });
     }
